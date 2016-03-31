de7dcf356662ddd028913f3726828f97adeb7cdf
Hewan Abreha <henebys@gmail.com>
2016-03-31 13:09:51 -0400

    made some changes with the layout

--- memory.js
+++ memory.js
@@ -1,18 +1,19 @@
+// created an array of colors to be randomly chosen.
 var colors = ["#01579b", "#01579b", "#ff6600", "#ff6600", "#A3E496", "#A3E496", "#303f9f", "#303f9f", "#FFCC33", "#FFCC33", "#C51162", "#C51162", "#33691e", "#33691e", "#CC6699", "#CC6699", "#51041B", "#51041B" , "#3e2723", "#3e2723"];
 
-
+//set clicks to 0
 var clicks = 0;
-
-// 1. 'select' the correct square element
+// selecting all classes named to cars'
 var elements = document.querySelectorAll(".cards");
+//setting globall variables
 var colorEven;
 var colorOdd;
 var firstCardOfPair;
 var secondCardOfPair;
 var currentCard;
-
+// calling color to shuffle
 shuffle(colors);
-
+//assigning a color to each card and puted it in eventlistner to show card
 function game() {
   for (var i = 0; i < elements.length; i++) {
     elements[i].setAttribute("name", colors[i]);
@@ -26,10 +27,10 @@ game();
 //
 
 function showCard(){
-  var bgColor = this.getAttribute("name");
+  var bgColor = this.getAttribute("name");//
   var id=this.getAttribute("id");
   this.style.background = bgColor;
-  if(clicks % 2 === 0) {
+  if(clicks % 2 === 0) { //
     colorEven = bgColor;
     firstCardOfPair = id;
   }
@@ -39,7 +40,7 @@ function showCard(){
   }
   if(clicks % 2 !== 0){
     if(colorEven !== colorOdd) {
-      var delay = 700; // This will make mismatched cards flip back over after 1 secondbox
+      var delay = 700; // This will make mismatched cards flip back over after 1 second.
       console.log("It's a mismatch");
       setTimeout(function(){
         document.getElementById(firstCardOfPair).style.background = '#263238';
@@ -52,7 +53,7 @@ function showCard(){
   clicks++;
   console.log(clicks);
 }
-
+// function to shuffle colors
 function shuffle(array) {
   var i;
   var j;
