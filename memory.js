700835a1ed2c2403496a2568c2a511ee5ccf5fa2
Hewan Abreha <henebys@gmail.com>
2016-08-07 16:14:28 -0400

    take out the duplicate code and fix errores

--- memory.js
+++ memory.js
@@ -1,61 +1,70 @@
 // created an array of colors to be randomly chosen.
 var colors = ["#01579b", "#01579b", "#ff6600", "#ff6600", "#A3E496", "#A3E496", "#303f9f", "#303f9f", "#FFCC33", "#FFCC33", "#C51162", "#C51162", "#33691e", "#33691e", "#CC6699", "#CC6699", "#51041B", "#51041B" , "#3e2723", "#3e2723"];
 
-//set clicks to 0
-var clicks = 0;
-// selecting all classes named to cars'
+// 'select' the correct square element.
 var elements = document.querySelectorAll(".cards");
 //setting globall variables
-var colorEven;
-var colorOdd;
+var colorOne;
+var colorTwo;
 var firstCardOfPair;
 var secondCardOfPair;
 var currentCard;
+
 // calling color to shuffle
 shuffle(colors);
-//assigning a color to each card and puted it in eventlistner to show card
+
 function game() {
   for (var i = 0; i < elements.length; i++) {
-    elements[i].style.background = '#263238';
     elements[i].setAttribute("name", colors[i]);
     elements[i].addEventListener("click", showCard);
   }
-
 }
-
 game();
 
 // <div name="whatever" id="something">
 // this.getAttribute("name")
-//
 
 function showCard(){
-  var bgColor = this.getAttribute("name");//
+  var bgColor = this.getAttribute("name");
   var id=this.getAttribute("id");
   this.style.background = bgColor;
-  if(clicks % 2 === 0) { //
-    colorEven = bgColor;
+
+  //First card pick logic
+  if(!colorOne) {
+    colorOne = bgColor;
     firstCardOfPair = id;
-  }
-  else {
-    colorOdd = bgColor;
+
+    // Second card pick logic
+  }else if (!colorTwo) {
+    colorTwo = bgColor;
     secondCardOfPair = id;
-  }
-  if(clicks % 2 !== 0){
-    if(colorEven !== colorOdd) {
-      var delay = 700; // This will make mismatched cards flip back over after 1 second.
-      console.log("It's a mismatch");
-      setTimeout(function(){
-        document.getElementById(firstCardOfPair).style.background = '#263238';
-        document.getElementById(secondCardOfPair).style.background = '#263238';
-      }, delay);
-    }else{
+
+    var colorMatch = (colorOne === colorTwo);
+    var diffCard = (firstCardOfPair !== secondCardOfPair)
+
+    if(colorMatch && diffCard) {
       console.log("It's a match!");
+      // Reset color variables
+      colorOne = undefined;
+      colorTwo = undefined;
+    }
+    else {
+      console.log("It's a mismatch");
+
+
+      var delay = 700; // This will make mismatched cards flip back over after 1 secondbox.
+    setTimeout(function(){
+      document.getElementById(firstCardOfPair).style.background = '#263238';
+      document.getElementById(secondCardOfPair).style.background = '#263238';
+    }, delay);
+
+    //Resets color variables
+    colorOne = undefined;
+    colorTwo = undefined;
     }
   }
-  clicks++;
-  console.log(clicks);
 }
+
 // function to shuffle colors
 function shuffle(array) {
   var i;
@@ -72,14 +81,9 @@ function shuffle(array) {
 var resetButton = document.querySelector("#shuffle");
 resetButton.addEventListener("click", resetGame );
 
-/// make a function resetGame
+// make a function resetGame
 function resetGame() {
-game();
-document.getElementById(firstCardOfPair).style.background = '#263238';
-document.getElementById(secondCardOfPair).style.background = '#263238';
+  game();
+  document.getElementById(firstCardOfPair).style.background = '#263238';
+  document.getElementById(secondCardOfPair).style.background = '#263238';
 }
-
-// step 1: it does nothing but console log
-// step 2: it shuffles cards array (but doesn't change the page)
-// step 3: it re-associates the card elements with the newly shuffled colors
-// step 4: do any variables need to be reset to default values (e.g. clicks)
